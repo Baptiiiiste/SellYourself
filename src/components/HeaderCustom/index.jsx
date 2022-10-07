3dfc7372ca02702bcadc8a07885eb4c818946113
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2022-10-07 15:37:50 +0200

    Création page connexion

--- src/components/HeaderCustom/index.jsx
+++ src/components/HeaderCustom/index.jsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
 import './headerCustom.css'
 function HeaderCustom({title}) {
+
+  if(title == "logForm") return(
+    <header>
+      <div className="headercustom-logo">
+        <img src={require('../../assets/Logo.png')} alt=""/>
+        <h1>SellYourself</h1>
+        <h1 className='headercustom-point'>.</h1>
+        <h1>fr</h1>
+      </div>
+      <div>
+        <Link to="/">Accueil</Link>
+      </div>
+    </header>
+  )
+
   return (
     <header>
       <div className="headercustom-logo">
@@ -9,8 +25,7 @@ function HeaderCustom({title}) {
         <h1>fr</h1>
       </div>
       <div className='headercustom-title'>
-        {/* <h1>{title}</h1> */}
-        <h1>title</h1>
+        <h1>{title}</h1>
       </div>
     </header>
   );
---
+++ src/pages/Connexion/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderCustom from '../../components/HeaderCustom';
+
+function Home() {
+  return(
+    <div className="App">
+      <HeaderCustom title="logForm"/>
+    </div>
+    )
+}
+
+export default Home;
