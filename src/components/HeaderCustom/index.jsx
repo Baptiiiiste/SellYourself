c61dd98007292bfd08ebc32b926a26f8d8baed78
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2022-10-07 10:03:56 +0200

    continue headercustom

--- src/components/HeaderCustom/index.jsx
+++ src/components/HeaderCustom/index.jsx
@@ -4,7 +4,9 @@ function HeaderCustom({title}) {
     <header>
       <div className="headercustom-logo">
         <img src={require('../../assets/Logo.png')} alt=""/>
-        <h1>SellYourself.fr</h1>
+        <h1>SellYourself</h1>
+        <h1 className='headercustom-point'>.</h1>
+        <h1>fr</h1>
       </div>
       <div className='headercustom-title'>
         {/* <h1>{title}</h1> */}
