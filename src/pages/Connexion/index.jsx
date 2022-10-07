224eddd10f04e25c6d5c08b762589e659c619f6e
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-10-07 17:13:59 +0200

    Connexion page

--- src/pages/Connexion/index.jsx
+++ src/pages/Connexion/index.jsx
@@ -1,12 +1,20 @@
+import './Connexion.css';
 import React from 'react';
 import FormulaireConnexion from '../../components/FormulaireConnexion';
+import HeaderCustom from '../../components/HeaderCustom';
 
 function Connexion() {
   return(
-    <div className="App">
-      <FormulaireConnexion />
+    <div className="Connexion">
+      <HeaderCustom title="logForm"/>
+      <div className='Connexion-bas'>
+        <FormulaireConnexion className="Connexion-form"/>
+        <div className="Connexion-entrediv"></div>
+        <div className="Connexion-image"></div>
+      </div>
+      
     </div>
-    )
+  )
 }
 
 export default Connexion;
