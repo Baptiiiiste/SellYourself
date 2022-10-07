484794bcd7bb371d7e1a068145b284868c70b97e
Hugo ROCHELLE <Hugo.ROCHELLE@etu.uca.fr>
2022-10-07 09:58:17 +0200

    Début left bar

--- src/components/LeftBar/index.jsx
+++ src/components/LeftBar/index.jsx
@@ -1,12 +1,28 @@
 import { Link } from 'react-router-dom';
 
 function Leftbar() {
-  return (
+  return true ?
+  (
+    <div className='allLeftBar'>
+      <div className='LeftBar-NameImg'>
+        <div className='LeftBar-ProfilePic'>
+          <img src={require('../../assets/DefaultPP.jpeg')} alt=""/>
+        </div>
+        <div className='Name'>
+          <p>
+            Utilisateur
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+  :
+  (
     <div>
         <Link to="/">Home</Link>
         <Link to="thisPageGoesNowhere">erreur</Link>
     </div>
-  );
+  )
 }
 
 export default Leftbar;
