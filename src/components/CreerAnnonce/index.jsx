520e9beef168d6f4341a4453e8f2ac8b20b1d84c
Eloan ANDRÉ <Eloan.ANDRE@etu.uca.fr>
2022-10-10 11:43:21 +0200

    Page publier amelioration 1

--- src/components/CreerAnnonce/index.jsx
+++ src/components/CreerAnnonce/index.jsx
@@ -2,10 +2,31 @@ import './creerAnnonce.css';
 
 function CreerAnnonce() {
     return(
-        <div className="principale">
-            <input type="text" placeholder='Titre' id="Titre" required name="Titre" maxlength="50"  />
-            <textarea placeholder='Description' id="Description" required name="Titre" maxlength="250" />
+        <div className="CreerAnnonce-principale">
+            <div className="CreerAnnonce-Input">
+                <input type="text" placeholder='Titre' id="CreerAnnonce-Titre" required name="Titre" maxlength="50"  />
+                <textarea placeholder='Description' id="CreerAnnonce-Description" required name="Description" maxlength="50"/>
+                <input type="text" placeholder='Prix (en euros)' id="CreerAnnonce-Prix" required name="Prix" maxLength="7"/>
+                <select name="Categorie" id="CreerAnnonce-Categorie">
+                    <option value="">--Choisissez une catégorie--</option>
+                    <option value="1">Catégorie 1</option>
+                    <option value="2">Catégorie 2</option>
+                    <option value="3">Catégorie 3</option>
+                    <option value="4">Catégorie 4</option>
+                    <option value="5">Catégorie 5</option>
+                    <option value="6">Catégorie 6</option>
+                </select>
+                {/* Mettre Les images ajoutés */}
+                <input type="file" placeholder='Ajouter une photo' id="CreerAnnonce-Image" required name="Image" accept="image/png, image/jpeg image/jpg" multiple/>
+
+                <p>Format .png .jpeg et .jpg uniquement</p>
+            </div>
+
+            <div className='CreerAnnonce-BoutonSubmit'>
+                <input type='submit' value="Publier l'annonce" />
+            </div>
         </div>
+
     )
 }
 
