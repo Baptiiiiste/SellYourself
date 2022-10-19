849517488a9f521d745410352728d3780f3bcbcd
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2022-10-19 14:36:58 +0200

    Mise en place de la connexion

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,34 @@ const app = express();
 require('./configuration/config');
 const cors = require('cors');
 const { User } = require("./configuration/models");
-// var bcrypt = require('bcryptjs');
+var bcrypt = require('bcryptjs');
 app.use(express.json());
 app.use(cors());
 
 app.post("/inscription", async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     resp.send(result);
 })
 
-// app.post("/connexion", async (req, resp) => {
-//     User.findOne({ pseudo: req.body.pseudo }, function(err, user) {
-//         if (err) throw err;
-//         bcrypt.compare(req.body.password, user.password, function(err, res) {
-//             if (err){
-//               console.log("erreur ---------------------------- ")
-//             }
-//             if (res) {
-//               // Send JWT
-//             } else {
-//               return resp.send({success: false, message: 'passwords do not match'});
-//             }
-//           });
-//     });
-// })
+app.post("/connexion", async (req, resp) => {
+    if(req.body.password && req.body.pseudo){
+        
+        let user = await User.findOne({pseudo: req.body.pseudo});
+        if(user){
+            if(bcrypt.compareSync(req.body.password, user.password)){
+                result = user.toObject();
+                delete result.password;
+                resp.send(result);
+            }else resp.send({result:"Wrong password"})
+        }else {
+            resp.send({result:"No data found"})
+        }
+    }else {
+        resp.send({result:"No data found"})
+    }
+})
 
 app.listen(5000);
