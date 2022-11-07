798a2749abc42ec7e49f327477e2025ac8698564
Loris PERRET <Loris.PERRET@etu.uca.fr>
2022-11-07 20:53:48 +0100

    afficher annonce

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,53 +6,6 @@ import LeftBar from '../../components/LeftBar';
 import HeaderCategories from '../../components/HeaderCategories/index.jsx';
 import UneAnnonce from '../../components/UneAnnonce';
 
-// const annonces = [
-//   {titre:'test annonce titre',
-//   description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-//   prix:27,
-//   img_annonce:'DefaultPP.jpeg',
-//   nom:'test nom',
-//   prenom:'test prenom', 
-//   img_profil:'DefaultPP.jpeg', 
-//   note:4.5},
-
-//   {titre:'test annonce titre',
-//   description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-//   prix:27,
-//   img_annonce:'DefaultPP.jpeg',
-//   nom:'test nom',
-//   prenom:'test prenom', 
-//   img_profil:'DefaultPP.jpeg', 
-//   note:4.5},
-
-//   {titre:'test annonce titre',
-//   description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-//   prix:27,
-//   img_annonce:'DefaultPP.jpeg',
-//   nom:'test nom',
-//   prenom:'test prenom', 
-//   img_profil:'DefaultPP.jpeg', 
-//   note:4.5},
-
-//   {titre:'test annonce titre',
-//   description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-//   prix:27,
-//   img_annonce:'DefaultPP.jpeg',
-//   nom:'test nom',
-//   prenom:'test prenom', 
-//   img_profil:'DefaultPP.jpeg', 
-//   note:4.5},
-
-//   {titre:'test annonce titre',
-//   description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-//   prix:27,
-//   img_annonce:'DefaultPP.jpeg',
-//   nom:'test nom',
-//   prenom:'test prenom', 
-//   img_profil:'DefaultPP.jpeg', 
-//   note:4.5}
-// ]
-
 function Home() {
 
   const [loader, setLoader] = useState(true);
@@ -79,27 +32,30 @@ function Home() {
   const getUtilisateur = async (pseudo) => {
     let result = await fetch(`http://localhost:5000/api/utilisateur/${pseudo}`);
     result = await result.json();
-    return(result);
+    return([result[0].nom, result[0].prenom, result[0].profilPic, result[0].note]);
+  }
+  
+  let lesAnnonces = [];
+  
+  const displayAnnonces = (listAnnonces) => {
+
+    annonces.forEach(async (item, index)=>{
+      const user = await getUtilisateur(item.utilisateur);
+      listAnnonces.push(<UneAnnonce titre={item.titre}
+                                  description={item.description}
+                                  prix={item.prix}
+                                  img_annonce={item.img_annonce} 
+                                  nom={user[0].nom}
+                                  prenom={user[0].prenom} 
+                                  img_profil={user[0].profilPic} 
+                                  note={user[0].note}
+                                  key={item.index}
+                                />);
+    })
   }
 
-  const annonce = [];
-
-  const displayAnnonces = async () => {
-    console.log('oui');
-
-    for( let i = 0; i < annonces.length; i++){
-      const user = getUtilisateur(annonces[i].utilisateur);
-
-    //   annonce.push({titre:'test annonce titre',
-    //     description:'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO',
-    //     prix:27,
-    //     img_annonce:'DefaultPP.jpeg',
-    //     nom:'test nom',
-    //     prenom:'test prenom', 
-    //     img_profil:'DefaultPP.jpeg', 
-    //     note:4.5});
-    }
-  }
+  console.log(lesAnnonces);
+  
 
   return loader ? 
     (
@@ -107,7 +63,7 @@ function Home() {
     )
     :
     (    
-    <div className="Home" onLoad={displayAnnonces}>
+    <div className="Home" onLoad={displayAnnonces(lesAnnonces)}>
       <LeftBar/>
       <div className='Home-center'>
         <div className='Home-header'>
@@ -123,18 +79,7 @@ function Home() {
           </div>
           
           <div className='Home-lesAnnonces'>
-            {/* {annonce.map((item, index) => (
-              <UneAnnonce titre={item.titre}
-                description={item.description}
-                prix={item.prix}
-                img_annonce={item.img_annonce} 
-                nom={item.nom}
-                prenom={item.prenom} 
-                img_profil={item.img_profil} 
-                note={item.note}
-                key={item.index}
-              />
-            ))} */}
+            {lesAnnonces}
           </div>
         </div>
         
