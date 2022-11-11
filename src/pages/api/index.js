506b0c245ae8829cfa18224befc03fd8dce957e3
Baptiste BONNEAU <Baptiste.BONNEAU@etu.uca.fr>
2022-11-11 15:46:28 +0100

    Edit profil

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -174,7 +174,7 @@ app.delete("/api/annonce/deleteAds/:idUser/:idAds", async (req, resp) => {
 });
 
 // Requete pour obtenir la liste des annonces d'un utilisateur précis
-app.get("/api/utilisateur/getAds/:pseudo", async (req, resp) => {
+app.get("/api/utilisateur/getAnnonces/:pseudo", async (req, resp) => {
     const user = await User.findOne({pseudo : req.params.pseudo});
     if(user){
 
