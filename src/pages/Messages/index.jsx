975ba63c50bedbec67d856b09e255dd2a35d82bd
loperret3 <loris.perret@etu.uca.fr>
2023-01-18 14:32:07 +0100

    message

--- src/pages/Messages/index.jsx
+++ src/pages/Messages/index.jsx
@@ -6,54 +6,50 @@ import Message from "../../components/Message";
 import LeftBar from "../../components/LeftBar";
 import { Link } from "react-router-dom";
 
-
 // Page message
 function Messages() {
 
-   // Utilisateur connecté
-   let connectedUser = sessionStorage.getItem("user");
+    // Variables
+    let connectedUser = sessionStorage.getItem("user");
+    const [messages, setMessages] = useState([]);
 
-   // Appel de fonction pour récupérer les annonces de l'utilisateur
-   useEffect(() => {
-       getUserConv();
-   }, [])
+    useEffect(() => {
+        getUserConv();
+    }, [])
 
-   const [messages, setMessages] = useState([]);
+    // Fonction pour récupérer les annonces de l'utilisateur
+    const getUserConv = async () => {
+        let listAds = [];
+        for(let i = 0; i < (JSON.parse(connectedUser).conversations).length; i++){
+            let a = await fetch(`https://api.sellyourself.fr/api/chat/${JSON.parse(connectedUser).conversations[i]}/${JSON.parse(connectedUser).pseudo}`, {
+                method: "Get",
+                headers: {
+                    'Content-Type': 'Application/json',
 
-   // Fonction pour récupérer les annonces de l'utilisateur à l'aide d'une requête vers l'api
-   const getUserConv = async () => {
-       let listAds = [];
-       for(let i = 0; i < (JSON.parse(connectedUser).conversations).length; i++){
-           let a = await fetch(`https://api.sellyourself.fr/api/chat/${JSON.parse(connectedUser).conversations[i]}/${JSON.parse(connectedUser).pseudo}`, {
-               method: "Get",
-               headers: {
-                   'Content-Type': 'Application/json',
+                    authorization: `bearer ${JSON.parse(sessionStorage.getItem('token'))}`
+                }
+            });
+            a = await a.json().then(a => listAds.push(a));
+        }
+        setMessages(listAds)
+    }
 
-                   authorization: `bearer ${JSON.parse(sessionStorage.getItem('token'))}`
-               }
-           });
-           a = await a.json().then(a => listAds.push(a));
-       }
-       setMessages(listAds)
-   }
-   
-   // fonction pour afficher 1 annonce de l'utilisateur
-   const displayConv = (item, index) => {
-       const annonce = item;
-   
-       return (
-       <Link to={`/chat/${annonce.idAnnonce}/${annonce.vendeur}/${annonce.acheteur}`}>
-        <Message
-            photo={annonce.otherPhoto}
-            pseudo={annonce.otherPseudo}
-            text={annonce.nomAnnonce}
-        />
-       </Link>
-       )
-   }
+    // fonction pour afficher 1 annonce de l'utilisateur
+    const displayConv = (item, index) => {
+        const annonce = item;
+        return (
+            <Link to={`/chat/${annonce.idAnnonce}/${annonce.vendeur}/${annonce.acheteur}`} style={"text-decoration: none"}>
+            <Message
+                photo={annonce.otherPhoto}
+                pseudo={annonce.otherPseudo}
+                text={annonce.nomAnnonce}
+            />
+            </Link>
+        )
+    }
     
 
-
+    // Affichage HTML
     return (
         <div className='Messages'>
             <LeftBar/>
